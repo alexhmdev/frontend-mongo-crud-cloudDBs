f4f0876b145b63672a017f4332e7e23f5ab806af
AlexHM <alex13pks@gmail.com>
2020-03-15 17:39:48 -0600

    Finished Get(with filters too), Post, Put and delete of customers

--- src/app/services/service.service.ts
+++ src/app/services/service.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { CustomerModel } from '../models/customer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
 
-  constructor() { }
+  private url:string = "http://localhost:3000/";
+
+  constructor(public http:HttpClient) { }
+
+
+  getCustomers(){
+    return this.http.get(this.url + 'customer/obtener').toPromise();
+  }
+
+  getCustomersFilterId(filter: any){
+    return this.http.get(`${this.url}customer/obtener/${filter}`).toPromise();
+  }
+  getCustomersFilterName(filter: any){
+    return this.http.get(`${this.url}customer/obtenerXnombre/${filter}`).toPromise();
+  }
+
+  getCustomersFilterCountry(filter: any){
+    return this.http.get(`${this.url}customer/obtenerXpais/${filter}`).toPromise();
+  }
+
+  deleteCustomer(id: Number){
+    return this.http.delete(`${this.url}customer/eliminar/${id}`).toPromise();
+  }
+
+  postCustomer(bodyCustomer: any){
+    return this.http.post(`${this.url}customer/registrar`,bodyCustomer).toPromise();
+  }
+
+  putCustomer(bodyCustomer: any, id: any){
+    return this.http.put(`${this.url}customer/actualizar/${id}`,bodyCustomer).toPromise();
+  }
+
 }
